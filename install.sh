ede0a921203f95528bc308f389b6999ecf250944
JefvdA <jef.v.d.a@live.be>
2023-10-31 23:10:49 +0100

    feat: clone bash-aliases and import

--- install.sh
+++ install.sh
@@ -2,6 +2,8 @@
 
 packages="neovim kitty i3"
 
+echo "\033[0;31mInstalling packages...\033[0m"
+
 for package in $packages; do
     # Use apt to install the package
     sudo apt install -y "$package"
@@ -14,3 +16,14 @@ for package in $packages; do
     fi
 done
 
+echo "\033[0;31mAll packages installed...\033[0m"
+
+BASH_ALIASES_DIR=$HOME/.bash-aliases
+
+if [ ! -d $BASH_ALIASES_DIR ]
+then
+    git clone git@github.com:JefvdA/bash-aliases.git $BASH_ALIASES_DIR
+    echo "# Add bash aliases from my config - JefvdA\nsource $BASH_ALIASES_DIR/aliases.sh\n" >> $HOME/.bashrc
+else
+    echo "\033[0;31mThe bash aliases was already cloned - Skipped this\033[0m"
+fi
