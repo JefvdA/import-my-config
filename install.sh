765c06c2b1e44aba87594bcba3073b0ac99e44da
JefvdA <jef.v.d.a@live.be>
2023-11-12 14:11:56 +0100

    docs: better ui ouput when running install

--- install.sh
+++ install.sh
@@ -16,14 +16,16 @@ for package in $packages; do
     fi
 done
 
-echo "\033[0;31mAll packages installed...\033[0m"
+echo "\033[0;31mAll packages installed!\033[0m"
 
 BASH_ALIASES_DIR=$HOME/.bash-aliases
 
 if [ ! -d $BASH_ALIASES_DIR ]
 then
+    echo "\033[0;31mInstalling bash aliases...\033[0m"
     git clone git@github.com:JefvdA/bash-aliases.git $BASH_ALIASES_DIR
     echo "# Add bash aliases from my config - JefvdA\nsource $BASH_ALIASES_DIR/aliases.sh\n" >> $HOME/.bashrc
+    echo "\033[0;31mThe bash aliases are cloned & are imported in your .bashrc !\033[0m"
 else
-    echo "\033[0;31mThe bash aliases was already cloned - Skipped this\033[0m"
+    echo "\033[0;31mThe bash aliases were already cloned - Skipped this\033[0m"
 fi
